425d7c2b767dc44aad0aa713c59931a7bf738c30
Daniel Ferreira <locopornerds@gmail.com>
2023-05-20 23:46:17 -0300

    finished

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+document.onreadystatechange = function() {
+    if (document.readyState == "complete") {
+        window.scrollTo(0, 0);
+    };
+};
+window.scrollTo(0, 0);
+
+
+function home() {
+    window.scrollTo(0, 0);
+
+  }
+
+  function projects() {
+    window.scrollTo(820, 820);
+
+  }
+
+  function about() {
+    window.scrollTo(12200, 182200);
+
+  }
